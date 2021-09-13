7d11d9b6e0b53f02e892f05cec22783dd416cd1e
Kamal Eladly <kamaleladly90@gmail.com>
2021-09-13 18:38:33 +0200

    last commmit

--- file.js
+++ file.js
@@ -1,36 +1,77 @@
-let btnCreate=document.querySelector('header .container >button')
-let createBage=document.querySelector('.create')
-let closeIcon=document.querySelector('.icon-close')
-let closeButton=document.querySelector('.create .container >button')
-let addButton=document.querySelector('.create .container .box button')
-let cardBox=document.querySelector('header .container .card1')
-let cardDelete=document.querySelector('header .container .card1 button')
-let box=document.querySelector('header .container .box')
-btnCreate.onclick = function (){
-    
-     createBage.classList.remove('hide')
- }
-closeIcon.onclick = function () {
-    createBage.classList.add('hide')
+let btnAdd=document.getElementById('btnAdd')
+let noData=document.getElementById('noData')
+let tasksBox=document.getElementById('tasksBox')
+let addInput=document.getElementById('addInput')
+let closeBtn=document.getElementById('closeBtn')
+let closeIcon=document.getElementById('closeIcon')
+let model=document.getElementById('model')
+let add=document.getElementById('add')
+let alert1=document.getElementById('alert1')
+let alertClose=document.getElementById('alertClose')
+
+
+let showmodel=()=>{
+    model.classList.remove('none')
 }
-closeButton.onclick = function () {
-    createBage.classList.add('hide')
+add.addEventListener('click',showmodel)
+
+let hideModel=()=>{
+    model.classList.add('none')
+
+}
+closeIcon.addEventListener('click',hideModel)
+closeBtn.addEventListener('click',hideModel)
+let close=()=>{
+    alert1.classList.add('none')
 }
 
-addButton.onclick=function () {
-    if(document.querySelector('.create .container .box input').value==""){
-        alert('Their Is No Data Entred')
+alertClose.addEventListener('click',close)
+
+let addTask=()=>{
+
+    let data=addInput.value;
+    if(data == ""){
+        alert('their is no data enterd');
+        alert1.classList.remove('none')
+
     }
     else{
-        
-    createBage.classList.add('hide')
-    document.querySelector('header .container .card1 span').innerHTML=document.querySelector('.create .container .box input').value
-    cardBox.classList.remove('hide')
-    box.classList.add('hide')
+
+    tasksBox.innerHTML+=`<div style="padding:30px; text-transform:capitalize ;"class="alert alert-info">
+    ${data}
+    <button class="beleteBtn btn btn-danger float-right">Delete</button>
+    <div>`
+    addInput.value=""
+    noData.classList.add('none')
+    alert1.classList.add('none')
+    
+    }
+    
     
 }
+btnAdd.addEventListener('click',addTask)
 
+let noelement=()=>{
+    if(tasksBox.childElementCount == 0){
+        noData.classList.remove('none')
+    }
 }
-cardDelete.onclick=function () {
-    cardBox.classList.add('hide');
-}
+
+document.addEventListener('click',function(e) {
+    if(e.target.classList.contains('beleteBtn'))
+    e.target.parentElement.remove();
+    noelement();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
